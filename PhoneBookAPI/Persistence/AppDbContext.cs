00d925ac3d3fd7ac348f069f88c2fa6de9464ac2
Nursultan Bekishov <sepia04@proton.me>
2026-05-03 18:35:31 +0600

    Add unique constraint to phone number.

--- PhoneBookAPI/Persistence/AppDbContext.cs
+++ PhoneBookAPI/Persistence/AppDbContext.cs
@@ -7,4 +7,9 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Phone> Phones { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Phone>().HasIndex(ph => ph.PhoneNumber).IsUnique();
+    }
 }
---
+++ PhoneBookAPI/Persistence/Migrations/20260503123526_UniqueToPhoneNumberField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueToPhoneNumberField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Phones_PhoneNumber",
+                table: "Phones",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Phones_PhoneNumber",
+                table: "Phones");
+        }
+    }
+}
